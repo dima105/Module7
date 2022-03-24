50a554a69bc8bd899c20c00f6d1056f5da6fba2c
Dmitry_Kulik <dima27121985@gmail.com>
2022-03-24 15:38:46 +0300

    Добавьте файлы проекта.

---
+++ Module7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Module7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int [6];
+                for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"\n Введите элемент массива под индексом {i} : \t ");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(array);
+            Array.Reverse(array);
+
+            foreach (int i in array)
+            {
+                Console.WriteLine($"\n Полученный элемент массива под индексом {i} : \t " + i) ;
+            }
+        }
+    }      
+}
---
+++ Module7_2/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Module7_2
+{
+
+    class MaxElementArray
+    {
+        public static void Main(string[] args)
+        {
+            int[][] array = new int[][]
+            {
+        new int[] {13, 4, 1},
+        new int[] {1, 4, 13},
+        new int[] {4, 13, 1},
+            };
+
+            Max(3, array);
+        }
+        public static void Max(int row, int[][] arr)
+        {
+            int i = 0;
+            int max = 0;
+            int[] result = new int[row];
+            while (i < row)
+            {
+                for (int j = 0;
+                         j < arr[i].Length; j++)
+                {
+                    if (arr[i][j] > max)
+                    {
+                        max = arr[i][j];
+                    }
+                }
+                result[i] = max;
+                max = 0;
+                i++;
+
+            }
+            Results(result);
+
+        }
+        private static void Results(int[] result)
+        {
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"\n Максимальное значение ряда : \t " + result[i]);
+            }
+        }
+    }
+}
